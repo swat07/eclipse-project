39cbc6168a8a2136e0775e420d4926040805bcc0
Malviya <swati.malviya@ais.com>
2025-06-01 18:13:23 -0400

    new code

--- src/JavaProgram/FindDuplicateCharacters.java
+++ src/JavaProgram/FindDuplicateCharacters.java
@@ -8,18 +8,6 @@ public class FindDuplicateCharacters {
 	public static void main(String[] args) {
 		String s1 = "swwattieerss";
 		ArrayList<Character> arrList = new ArrayList<>();
-		
-//		
-//		char s2[] = s1.toCharArray();
-//		for(int i=0; i<s2.length; i++) {
-//			for(int j=i+1; j<s2.length; j++) {
-//				if(s2[i] == s2[j]) {
-//					arrList.add(s2[i]);
-//				}
-//			}
-//		}
-//		System.out.print("Duplicate characters are: " +arrList);
-		
 		HashMap<Character, Integer> hm = new HashMap<>();
 		for(int i=0; i<s1.length(); i++) {
 			char key = s1.charAt(i);
